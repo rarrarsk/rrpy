88b5b8b46dd1a0c54938d9e0bb8542feafe2b020
rarrarsk <125333263+rarrarsk@users.noreply.github.com>
2023-03-10 13:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://rarrarsk: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@github.com/rarrarsk/Gangster-Baby-Renamer-V2 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://rarrarsk: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@github.com/rarrarsk/Rename-bot- ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
