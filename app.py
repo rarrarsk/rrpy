58bb093a5bb1941d6a290d465b9cc5560ad83b41
rarrarsk <125333263+rarrarsk@users.noreply.github.com>
2023-03-12 15:18:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://rarrarsk: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@github.com/rarrarsk/URLtoTG001 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://rarrarsk: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@github.com/rarrarsk/TorToolkit-Telegram ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m tortoolkit &")
